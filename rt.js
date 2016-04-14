4dcc3e661a9381b0d5a2e404d5f18704a0e384c9
Jeremiah Palmer <jpalmer0727+git@gmail.com>
2016-04-14 17:22:34 -0400

    Full rebuild to pull videos from Recently Added instead of jumping through individual show listings.  TODO: Build out Heroku server to move away from console application.  Create intelligent keyword search based on '-,_' url string separations. Site/show sorting. Accounts auth to limit full database update to admins.

--- rt.js
+++ rt.js
@@ -1,286 +1,260 @@
+// grab the packages we need
+var JsonDB = require('node-json-db');
 var request = require('request');
 var cheerio = require('cheerio');
 var prompt = require('prompt');
 var colors = require('colors');
 var open = require('open');
 
+const fs = require('fs');
 
-var searchWord = "";
-var showArray = [];
-var seasonArray = [];
-var episodeList = [];
-var pagesToVisit = [];
-var count = 0;
+prompt.message = '';
 
-var siteArray = [
-            // Base URL
-            ['http://roosterteeth.com',
-            // Search terms
-            ['rooster','rt','rooster teeth','roosterteeth','teeth']
-        ],
-            // Base URL
-            ['http://achievementhunter.com',
-            // Search terms
-            ['hunter','ah','achieve','achievement','achievementhunter']
-        ],
-            // Base URL
-            ['http://fun.haus',
-            // Search terms
-            ['funhaus','fh','fun','haus']
-        ],
-            // Base URL
-            ['http://screwattack.com',
-            // Search terms
-            ['screw','sa','screw attack','attack','screwattack']
-        ],
-            // Base URL
-            ['http://theknow.tv',
-            // Search terms
-            ['know','the know','theknow']
-        ]
-    ];
+var promptType = ['integer','string']; // promptType[0, 1]
 
-init();
+var baseURL = 'http://roosterteeth.com/episode/recently-added?page=';
+var pageArray = [];
+var resultsArray = [];
+var linkArray = [];
+var updateArray = [];
+var pageCount = 0;
+var updateOnly = false;
+var time;
+var lastPage;
 
-function init() {
-    console.log(colors.cyan('List of sites: '));
-    console.log('    Rooster Teeth');
-    console.log('    Achievement Hunter');
-    console.log('    Funhaus');
-    console.log('    ScrewAttack');
-    console.log('    The Know');
+var db = new JsonDB('rtArchive', true, true);
+var exists = fs.exists('rtArchive.json', (exists) => {
+    if (exists) {
+        linkArray = db.getData('/');
+        time = fs.statSync('rtArchive.json').mtime;
+    }
+    setup();
+});
+
+function setup() {
     prompt.start();
-      // 
-      // Get two properties from the user: username and email 
-      // 
+    console.log('\n    ' + colors.red.underline.bold('Rooster Teeth Video Search'));
+    console.log(colors.yellow('    Database last updated - ' + time));
+    init();
+}
+
+// Base initialization function
+function init() {
+    return basePrompt('    Make a choice', [
+        ['Perform Search?',searchDB],
+        ['Database Functions?',promptDB],
+        ['Exit Program',exit]
+    ], 0);
+}
+
+// Base prompt template
+function basePrompt(promptText, options, type) {
+    if (type === 0) {
+        for (var i = 0; i < options.length; i++) {
+            if (i > options.length - 3 || i === 0) {
+                console.log('');
+            }
+            console.log(colors.white("    " + (i + 1) + ") " + options[i][0]));
+        }
+    }
     prompt.get([{
-        name: 'site',
-        description: colors.blue('Which site is the show on?'),
-        type: 'string',
+        name: 'choice',
+        description: colors.blue(promptText),
+        type: promptType[type],
         required: true
-    }], function (err, result) {       
-        // 
-        // Log the results. 
-        //
-        for (var i = 0; i < siteArray.length; i++) {
-            var tempArray = siteArray[i][1];
-            var length = tempArray.length;
-            for (var j = 0; j < length; j++) {
-                var isSiteFound = searchForWord(result.site, tempArray[j]);
+    }], function (err, result) {     
+        if (type === 0) {  
+            if (result.choice > options.length || result.choice < 1) {
+                console.log(colors.red('    Invalid selection. Please choose again.'));
+                return basePrompt(promptText, options, type);
+            } else {
+                var row = options[result.choice - 1];
+                return row[row.length - 1](row);
+            }
+        } else if (type === 1) {
+            process.stdout.write(colors.cyan('\n    Searching...\n    This may take some time'));
+            resultsArray = [];
+            for (var i = 0; i < linkArray.length; i++) {
+                var isSiteFound = searchForWord(linkArray[i][1], result.choice);
                 if (isSiteFound) {
-                    console.log(colors.green('    Site Found: ' + siteArray[i][0]));
-                    return showList(siteArray[i][0]);
+                    process.stdout.write(colors.cyan('.'));
+                    var name = linkArray[i][0].replace('&#x2013;','-').replace('&apos;',"'").replace('&quot;','"').replace('&quot;','"');
+                    linkArray[i][0] = name;
+                    linkArray[i].push(openLink);
+                    resultsArray.push(linkArray[i]);
+                    // resultsArray.push([linkArray[i], openLink]);
                 }
             }
+            searchResults();
         }
-        console.log(colors.red('Unable to find site match. Please try again.'));
-        init();
     });
 }
 
-function showList(baseURL) {
-    console.log('');
-    console.log('Searching for shows...');
-
-    var searchURL = baseURL + "/show"
-    reqFunc(searchURL, collectShows);
+function openLink(episode) {
+    console.log(colors.magenta('    Opening ' + episode[0] + '\n    Please Wait...\n'));
+    open(episode[1]);
+    return basePrompt('    Would you like to search again?', [
+        ['Search Again',searchDB],
+        ['Exit Program',exit]
+    ], 0);
 }
 
-function visitPage(baseURL) {
-    console.log('');
-    console.log('Searching for episodes...');
+function promptDB() {
+    return basePrompt('    Make a choice', [
+        ['Update Database?',updateDB],
+        ['Rebuild Database (NOTE: Rebuilding can take awhile)?',areYouSure],
+        ['Exit Program',exit]
+    ], 0);
+}
 
-    var searchURL = baseURL + "/season"
-    reqFunc(searchURL, collectSeasons);
+function areYouSure() {
+    return basePrompt('    Are You Sure', [
+        ['Yes',buildDB],
+        ['No',init],
+        ['Exit Program',exit]
+    ], 0);
+}
+
+function updateDB() {
+    updateOnly = true;
+    buildDB();
+}
+
+function buildDB() {
+    var initUrl = baseURL + 1;
+    reqFunc(initUrl, parse, '    Initializing crawl...\n    This may take some time\n');
+}
+
+function searchDB() {
+    if (linkArray.length < 1) {
+        console.log(colors.red('    Database is empty.'));
+        return basePrompt('    Would you like to build it now?', [
+            ['Rebuild Database (NOTE: Rebuilding can take awhile)?',buildDB],
+            ['Close Program',exit]
+        ], 0);
+    } else {
+        return basePrompt('    Enter Search String', null, 1);
+    }
+}
+
+function searchResults() {
+    console.log(colors.magenta('\n    ' + resultsArray.length + ' matches found'));
+    resultsArray.push(['Search Again',searchDB]);
+    resultsArray.push(['Exit Program',exit]);
+    return basePrompt('    Make a choice', resultsArray, 0);
+}
+
+function exit() {
+    console.log(colors.bold.blue('    BYE!'));
+    return process.exit();
 }
 
 function searchForWord($, word) {
   return($.indexOf(word.toLowerCase()) !== -1);
 }
 
-function reqFunc(url, callback) {
+function reqFunc(url, callback, text) {
+    
+    process.stdout.write(colors.cyan(text));
 
     // Make the request
     request(url, function(error, response, body) {
         // Check status code (200 is HTTP OK)
-        if(response.statusCode !== 200) {
-            console.log("Status code: " + response.statusCode);
+        if(error || response.statusCode !== 200) {
+            console.log(colors.red("    Error received - Status code: " + response.statusCode));
             return;
         }
+
         // Parse the document body
         var $ = cheerio.load(body);
-        callback(url, $);
+        callback($);
     });
 }
 
-function collectShows(url, $) {
-    var showLinks = $("a[href^='" + url + "']");
-
-    showLinks.each(function() {
-        var name = $(this).children('.name').text();
-        var link = $(this).attr('href');
-        var isWordFound = searchForWord(link, url + '/');
-        if(isWordFound && name != '') {
-            showArray.push([name,link]);
-        }
-    });
-
-    showArray.sort();
-    var tempValue = showArray[0];
-    for (var i = 0; i < showArray.length; i++) {
-        if (tempValue != showArray[i + 1]) {
-            showArray.splice(i,1);
-        }
-        tempValue = showArray[i];
-    }
-
-
-    // List Shows
-    if (showArray.length < 1) {
-        return console.log(colors.red('Sorry! No shows found! '));
-    }
-
-    console.log(colors.green('Shows Found! '));
-    console.log('');
-
-    for (var i = 0; i < showArray.length; i++) {
-        console.log((i+1) + ") " + showArray[i][0]);
-    }
-    console.log('');
-
-    prompt.get([{
-        name: 'show',
-        description: colors.blue('Which show do you want to view episodes for?'),
-        required: true
-    }], function (err, result) {       
-        // 
-        // Log the results. 
-        //
-        if (result.show > showArray.length + 1 || result.show < 1) {
-            console.log(colors.red('Unable to find site match. Please try again.'));
-            init();
+// Parse request
+function parse($) {
+    if (pageCount === 0) {
+        if (!updateOnly) {
+            lastPage = $('ul.controls a').eq(-2).text();
         } else {
-            return visitPage(showArray[result.show-1][1]);
+            var tempTime = time.getDay();
+            var now = new Date().getDay();
+            var diff = now - tempTime;
+            lastPage = diff * 4 + 2;
         }
-    });
-}
-
-function collectSeasons(url, $) {
-    var seasonLinks = $("a[href^='" + url + "']");
-
-    seasonLinks.each(function() {
-        var link = $(this).attr('href');
-        seasonArray.push(link);
-    });
-
-    for (var i = 0; i < seasonArray.length; i++) {
-        reqFunc(seasonArray[i], collectEpisodes);
-    }
-}
-
-function collectEpisodes(url, $) {
-   count++;
-    var relativeLinks = $("a[href^='http']");
-    relativeLinks.each(function() {
-        var link = $(this).attr('href');
-        var isWordFound = searchForWord(link, "page=");
-        if(isWordFound) {
-           pagesToVisit.push(link);
-        }
-    });
-
-    if (count === seasonArray.length) {
-        searchEpisodes();    
-    }
-}
-
-function searchEpisodes() {
-    count = 0;
-    console.log(colors.green((pagesToVisit.length + 1) + " Pages Found"));
-    console.log('');
-    prompt.get([{
-        name: 'search',
-        description: colors.blue('What is your search keyword?'),
-        type: 'string',
-        required: true
-    }], function (err, result) { 
-        console.log('This may take some time...');
-        searchWord = result.search;
-        for (var i = 0; i < pagesToVisit.length; i++) {
-            reqFunc(pagesToVisit[i], listEpisodes);
-        }
-    });
-}
-
-function listEpisodes(url, $) {
-    count++;
-    var relativeLinks = $("a[href^='http']");
-    relativeLinks.each(function() {
-        var link = $(this).attr('href');
-        var name = $(this).children('.name').text();
-        var isWordFound = searchForWord(link, searchWord);
-        if(isWordFound) {
-           episodeList.push([name,link]);
-        }
-    });
-    // console.log('Found ' + (episodeList.length + 1) + ' episodes matching...searching...');
-
-    if (count === pagesToVisit.length) {
-        cleanUp();    
-    }
-}
-
-function cleanUp() {
-    if (episodeList.length > 0) {
-        console.log(colors.green('SUCCESS! ' + (episodeList.length + 1) + ' episodes found'));
-        console.log('');
-        episodeList.sort();
-
-        var tempValue = episodeList[0];
-        for (var i = 0; i < episodeList.length; i++) {
-            if (tempValue != episodeList[i + 1]) {
-                episodeList.splice(i,1);
-            }
-            tempValue = episodeList[i];
-        }
-
-        for (var i = 0; i < episodeList.length; i++) {
-            console.log((i+1) + ") " + episodeList[i][0] + "  -  " + episodeList[i][1]);
-        }
-        console.log('');
-        prompt.get([{
-            name: 'choice',
-            description: colors.cyan('Choose video to view:'),
-            type: 'string',
-            required: true
-        }], function (err, result) { 
-            if (result.choice > episodeList.length + 1 || result.choice < 1) {
-                console.log(colors.red('Unable to find site match. Please try again.'));
-                return searchEpisodes();
-            } else {
-                return open(episodeList[result.choice - 1][1]);
-            }
-        });
+        return linkBuilder();        
     } else {
+        baseLinks = $('main ul.episode-blocks a');
+        baseLinks.each(function() {
+            process.stdout.write(colors.cyan('.'));
+            var base = $(this);
+            var link = base.attr('href');
 
-        console.log('1) New Search?');
-        console.log('2) Start Over?');
-        prompt.get([{
-            name: 'choice',
-            description: colors.red('Unable to find any matches. New search or start over?'),
-            type: 'string',
-            required: true
-        }], function (err, result) { 
-            if (result.choice == 1) {
-                searchEpisodes();
-            } else if (result.choice == 2) {
-                init();
-            } else {
-                console.log(colors.red('Invalid Option'));
-            }
+            var name = base.children('.name').html();
+            var time = base.next().html();
+            var image = 'http://' + base.find($('img')).attr('src');
+            var sponsor = base.find($('.ion-star')).length > 0;
+            var length = base.find($('.timestamp')).text();
+
+            updateArray.push([name, link, time, image, sponsor, length]);
         });
     }
+    if (pageCount < pageArray.length) {
+        return linkBuilder();    
+    } else {
+        console.log(colors.cyan('\n    Build complete!'));
+        if (!updateOnly) {
+            linkArray = updateArray;
+            db.push('/',linkArray);
+            return init();
+        } else {
+            return updateDiff();
+        }
+    }
 }
+
+// Build array of links
+function linkBuilder() {
+    if (pageCount === 0) {
+        var tempUrl;
+        var tempArray = [];
+        for (var i = 1; i <= lastPage; i++) {
+            tempUrl = baseURL + i;
+            tempArray.push(tempUrl);
+        }
+        pageArray = tempArray;    
+    } 
+    return gatherEpisodes();
+}
+
+function gatherEpisodes() {
+    pageCount++;
+    return reqFunc(pageArray[pageCount - 1], parse, '\n    Building page ' + pageCount + ' episode list');
+}
+
+function updateDiff() {
+    var tempArray = [];
+    for (var i = 0; i < updateArray.length; i++) {
+        if (updateArray[i][1] != linkArray[0][1]) {
+            tempArray.push(updateArray[i]);
+        } else {
+            if (tempArray.length > 0) {
+                tempArray.reverse();
+                for (var i = 0; i < tempArray.length; i++) {
+                    linkArray.unshift(tempArray[i]);    
+                }
+                console.log(colors.yellow('    Database updated with ' + tempArray.length + ' links.'));
+                db.push('/',linkArray);                
+            } else {
+                console.log(colors.yellow('    No new links found!'));
+            }
+            break;
+        }
+    }
+    return init();
+}
+
+
 
 
 
